8aaa2a4f5c1d56f79e424c0c6f05d6eb5e85fa32
Jack Huang <593046857@.qq.com>
2021-03-23 14:06:04 +0800

    第三次提交到本地库

--- Test/Test01/src/main/java/com/example/People.java
+++ Test/Test01/src/main/java/com/example/People.java
@@ -2,10 +2,10 @@ package com.example;
 
 /**
  * @Author: JackHuang
- * @Date: 2021/3/23 - 03-23 - 13:53
+ * @Date: 2021/3/23 - 03-23 - 13:59
  * @Description: com.example
  */
-public class Student {
+public class People {
     private String name;
     private Integer age;
     private String address;
