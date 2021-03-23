fbd0573839370d54c3f34f40038f959dad7b28d3
Jack Huang <593046857@.qq.com>
2021-03-23 15:13:01 +0800

    第五次提交到本地库

---
+++ Test/Test01/src/main/java/com/example/Child.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * @Author: JackHuang
+ * @Date: 2021/3/23 - 03-23 - 14:16
+ * @Description: com.example
+ */
+public class Child {
+    private String name;
+    private Integer age;
+}
---
+++ Test/Test01/src/main/java/com/example/Teacher.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * @Author: JackHuang
+ * @Date: 2021/3/23 - 03-23 - 14:07
+ * @Description: com.example
+ */
+public class Teacher {
+    private String name;
+    private String age;
+}
