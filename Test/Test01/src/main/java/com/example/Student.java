2e84f2b8e18696b1ebe195d2698c7040bad0e537
Jack Huang <593046857@.qq.com>
2021-03-23 13:58:15 +0800

    第二次提交到本地库

---
+++ Test/Test01/src/main/java/com/example/Student.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * @Author: JackHuang
+ * @Date: 2021/3/23 - 03-23 - 13:53
+ * @Description: com.example
+ */
+public class Student {
+    private String name;
+    private Integer age;
+}
